eaa1a9c373bbac39db808b2807ca83fe347ecea2
RoseGiftCode <aigiftson@protonmail.com>
2024-08-28 15:54:16 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
+// next.config.mjs
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@chain': path.resolve('./src/chain.ts'), // Ensure this path matches the actual location
+      '@walletclient': path.resolve('./src/walletclient.ts'), // Ensure this path matches the actual location
+    };
+    return config;
+  },
 };
 
-export default  nextConfig;
+export default nextConfig;
+
